38d03257525e02bf47a4aacc07cf031911f1037e
Fernando Brandao <fwbrandao@gmail.com>
2018-03-08 16:35:08 +0000

    Implement view gigs i'm attending.

---
+++ GigHub/ViewModels/GigsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GigHub.Models;
+using System.Linq;
+
+namespace GigHub.ViewModels
+{
+    public class GigsViewModel
+    {
+        public IEnumerable<Gig> UpcomingGigs { get; set; }
+        public bool ShowActions { get; set; }
+        public string Heading { get; set; }
+
+    }
+}
